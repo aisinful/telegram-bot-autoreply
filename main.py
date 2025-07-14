98f40002ed3be874e123a58be54f75983eeb6037
Jorge <izenxyte@gmail.com>
2025-07-14 22:19:06 +0100

    funciona ya hostia

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import os
+import asyncio
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 
 async def auto_reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hola! Gracias por mandarme un mensaje 💕 Por desgracia recibo demasiados mensajes por aqui, si quieres hablar conmigo puedes contactarme en mi Fansly u Only link.me/kalullamas")
+    await update.message.reply_text("Hola! Gracias por mandarme un mensaje 💕 Por desgracia recibo demasiados mensajes por aquí. Si quieres hablar conmigo, puedes contactarme en mi Fansly u Only link.me/kalullamas")
 
-def main():
+async def main():
     BOT_TOKEN = os.getenv("BOT_TOKEN")
+    if not BOT_TOKEN:
+        raise ValueError("BOT_TOKEN no definido")
+
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, auto_reply))
-    app.run_polling()
+
+    # Ejecuta el bot en segundo plano
+    await app.initialize()
+    await app.start()
+    await app.updater.start_polling()
+
+    # Bucle para mantener vivo el proceso
+    while True:
+        await asyncio.sleep(60)
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
