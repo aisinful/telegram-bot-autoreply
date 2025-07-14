0af85f1077851231b6bc66869e6fd553e4250422
Jorge <izenxyte@gmail.com>
2025-07-14 21:12:48 +0100

    Updated to python-telegram-bot v20.7 to fix imghdr issue

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import os
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 
-def auto_reply(update, context):
-    chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id=chat_id, text="Hola! Gracias por mandarme un mensaje 💕 Por desgracia recibo demasiados mensajes por aqui, si quieres hablar conmigo puedes contactarme en mi Fansly u Only link.me/kalullamas")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def auto_reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hola! Gracias por mandarme un mensaje 💕 Por desgracia recibo demasiados mensajes por aqui, si quieres hablar conmigo puedes contactarme en mi Fansly u Only link.me/kalullamas")
 
 def main():
-    token = os.getenv("7871592696:AAFwxP5QCfx6HFDmnAS_38DmR3tFvEEIY3k")
-    updater = Updater(token=token, use_context=True)
-    dp = updater.dispatcher
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, auto_reply))
-    updater.start_polling()
-    updater.idle()
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, auto_reply))
+    app.run_polling()
 
 if __name__ == '__main__':
     main()
